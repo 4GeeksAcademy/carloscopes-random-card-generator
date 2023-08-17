06635b402eb0dfd58e11abeab45c41e7d32ada06
carloscopes <carlos.mcp.97@gmail.com>
2023-08-17 17:28:57 -0400

    :tada: First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,54 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+/*import "bootstrap";
+import "./style.css";*/
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  newCardGenerator();
+
+  function suitGenerator() {
+    let suites = ["♠", "♣", "♥", "♦"];
+    let randomSuitPosition = Math.floor(Math.random() * suites.length);
+    return suites[randomSuitPosition];
+  }
+
+  function numberGenerator() {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    let randomNumberPosition = Math.floor(Math.random() * numbers.length);
+    return numbers[randomNumberPosition];
+  }
+
+  const button = document.querySelector("#btn");
+  button.addEventListener("click", newCardGenerator);
+
+  function newCardGenerator() {
+    const numberCard = document.querySelector(".number");
+    let numberElement = numberGenerator();
+    numberCard.innerHTML = numberElement;
+
+    const suitCard = document.querySelectorAll("span");
+    let suitElement = suitGenerator();
+    suitCard.forEach(span => {
+      span.innerHTML = suitElement;
+      if (suitElement == "♦" || suitElement == "♥") {
+        span.style.color = "#E5446D";
+      }
+    });
+  }
+  window.setInterval(() => {
+    newCardGenerator();
+  }, 5000);
 };
